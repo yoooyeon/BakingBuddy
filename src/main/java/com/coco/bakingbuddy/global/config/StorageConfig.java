f5bdaa18786b27ac856c559e6a8ae4835be5e4c5
yoooyeon <yooyeon.dev@gmail.com>
2024-07-03 17:37:43 +0900

    feat: Google Cloud Storage 연동

---
+++ src/main/java/com/coco/bakingbuddy/global/config/StorageConfig.java
@@ -0,0 +1,14 @@
+package com.coco.bakingbuddy.global.config;
+
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StorageConfig {
+    @Bean
+    public Storage storage() {
+        return StorageOptions.getDefaultInstance().getService();
+    }
+}
