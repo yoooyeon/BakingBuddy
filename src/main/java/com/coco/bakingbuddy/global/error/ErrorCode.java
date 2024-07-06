ed8fda196519cd83acffe717dcc3196e97ab441c
yoooyeon <yooyeon.dev@gmail.com>
2024-07-06 21:23:52 +0900

    add: ErrorCode 추가

--- src/main/java/com/coco/bakingbuddy/global/error/ErrorCode.java
+++ src/main/java/com/coco/bakingbuddy/global/error/ErrorCode.java
@@ -26,7 +26,11 @@ public enum ErrorCode {
     INVALID_PASSWORD(BAD_REQUEST, "패스워드가 일치하지 않습니다."),
 
     //dir
-    DIR_NOT_FOUND(NOT_FOUND,"존재하지 않는 디렉토리")
+    DIR_NOT_FOUND(NOT_FOUND,"존재하지 않는 디렉토리"),
+
+
+    //recipe
+    RECIPE_NOT_FOUND(NOT_FOUND, "존재하지 않는 레시피")
     ;
 
     private final HttpStatus status;
