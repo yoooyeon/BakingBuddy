467cf0cb643557105f06e9c388f0fa5fd2b54d91
yoooyeon <yooyeon.dev@gmail.com>
2024-07-06 17:40:58 +0900

    feat: redis 연결

--- src/main/java/com/coco/bakingbuddy/BakingBuddyApplication.java
+++ src/main/java/com/coco/bakingbuddy/BakingBuddyApplication.java
@@ -2,9 +2,10 @@ package com.coco.bakingbuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+@EnableCaching
 @EnableAspectJAutoProxy
 @EnableJpaAuditing
 @SpringBootApplication
