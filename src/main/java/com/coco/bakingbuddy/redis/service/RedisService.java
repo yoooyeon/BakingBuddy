05686daf259e18dbde4af271691aa39092aec8c1
yoooyeon <yooyeon.dev@gmail.com>
2024-07-06 21:22:59 +0900

    update: 레디스 자동완성 수정

--- src/main/java/com/coco/bakingbuddy/redis/service/RedisService.java
+++ src/main/java/com/coco/bakingbuddy/redis/service/RedisService.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -31,26 +33,33 @@ public class RedisService {
         redisTemplate.delete(key);
     }
 
+
     // 검색어를 저장하는 메서드
+    @Transactional
     public void saveSearchTerm(String term) {
         List<String> names = recipeQueryDslRepository.findByName(term);
         if (names != null && !names.isEmpty() && names.size() > 0) {
-            String redisKey = "search_term:" + term;
-            log.info("redisKey=" + redisKey);
-            redisTemplate.opsForSet().add(redisKey, names.toArray(new String[0]));
-            // 로그 확인용
-            Set<Object> values = redisTemplate.opsForSet().members(redisKey);
-            log.info("Values for {}: {}", redisKey, values);
+            String redisKey = "search_autocomplete:" + term;
+            String[] values1 = names.toArray(new String[0]);
+            redisTemplate.opsForSet().add(redisKey, values1);
         }
     }
 
     // 자동완성을 위한 검색어 제안 메서드
     public Set<String> autocomplete(String prefix) {
-        // Redis의 Sorted Set을 이용하여 prefix로 시작하는 검색어를 찾아서 반환
-        // 예시로는 Set을 사용하도록 수정
-        return redisTemplate.keys("search_term:" + prefix + "*")
-                .stream()
-                .map(x -> x.substring("search_term:".length()))
-                .collect(Collectors.toSet());
+        String redisPattern = "search_autocomplete:" + prefix + "*";
+        Set<String> keys = redisTemplate.keys(redisPattern);
+        Set<String> autocompleteResults = new HashSet<>();
+        if (keys != null) {
+            for (String key : keys) {
+                Set<Object> values = redisTemplate.opsForSet().members(key);
+                if (values != null) {
+                    autocompleteResults.addAll(values.stream()
+                            .map(Object::toString)
+                            .collect(Collectors.toSet()));
+                }
+            }
+        }
+        return autocompleteResults;
     }
 }
